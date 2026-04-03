0e21bd82e075a69580994cbaa5fa55c9b06517e4
AkshatBaranwal-Works <akshatbaranwal3221@gmail.com>
2026-04-03 18:26:54 +0530

    Logic Operators
    Ex- Like switching Cuircit

---
+++ operators.py
@@ -0,0 +1,5 @@
+# Logic Operators
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
