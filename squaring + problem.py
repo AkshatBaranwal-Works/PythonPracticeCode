b9b14e2bf68c66ef4e1040dceedca155d7a5362a
AkshatBaranwal-Works <akshatbaranwal3221@gmail.com>
2026-04-05 21:43:59 +0530

    Squaring And Problems
    I have made code which squares any number 
    in this code the problem is python do not use (^) this as a squaring

---
+++ squaring + problem.py
@@ -0,0 +1,6 @@
+a = int(input("Enter Your number: "))
+
+
+print ("The square of the number is", a**2)
+print ("The square of the number is", a*a)
+# print ("The square of the number is", a^2)  python not accept this symbol for squaring 
